5878765879d5c24d7275f11bb44da362558741dc
Madiha Farhana <117527960+madihafarhana21@users.noreply.github.com>
2026-04-01 04:45:47 +0530

    changes made to app

--- app.py
+++ app.py
@@ -26,25 +26,19 @@ from fastai.vision.all import *
 import gradio as gr
 import pathlib
 import platform
+from PIL import Image as PilImg  # different alias to avoid fastai conflict
 
-# Fix 1: WindowsPath on Linux
 if platform.system() != 'Windows':
     pathlib.WindowsPath = pathlib.PosixPath
 
 learn = load_learner('model.pkl')
 categories = ('Black', 'Grizzly', 'Teddy')
 
-# def classify_bear(img):
-#     pred, idx, probs = learn.predict(img)
-#     return dict(zip(categories, map(float, probs)))
-
 def classify_bear(img):
-    # Defensively ensure we always have a clean PIL Image
     if isinstance(img, dict):
-        # Gradio sometimes wraps as {"image": <PIL>, ...}
         img = img.get("image") or img.get("composite") or list(img.values())[0]
-    if not isinstance(img, PILImage.Image):
-        img = PILImage.fromarray(img)
+    if not isinstance(img, PilImg.Image):  # use the new alias
+        img = PilImg.fromarray(img)
     
     pred, idx, probs = learn.predict(img)
     return dict(zip(categories, map(float, probs)))
@@ -58,6 +52,6 @@ intf = gr.Interface(
     inputs=image,
     outputs=label,
     examples=examples,
-    cache_examples=False   # Fix 2: disable caching that triggers the bug at startup
+    cache_examples=False
 )
 intf.launch(inline=False, share=True)
